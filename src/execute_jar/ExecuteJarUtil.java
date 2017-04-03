2fdef71e96a06ca63f929fe9d8402284e28d0ebf
dmeade3 <dcmstrat@gmail.com>
2017-04-03 07:52:35 -0400

    re

--- src/execute_jar/ExecuteJarUtil.java
+++ src/execute_jar/ExecuteJarUtil.java
@@ -1,5 +1,8 @@
 package execute_jar;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Project: Excavator
  * File Name: ExecuteJarUtil.java
@@ -9,26 +12,48 @@ package execute_jar;
 public class ExecuteJarUtil
 {
 
-	// TODO move everything from other parts of the program that have to do with executing and com
+	// TODO make the other paths consist more of variables
 
 
 	////// PATHS - PATH HELPERS //////
-	public static final String PROJECT_DIR = System.getProperty("user.dir");
 	public static final String USER_NAME = System.getProperty("user.name");
-	public static String OUTSIDE_PROGRAM_JAR_PATH = "C:\\Users\\" + USER_NAME + "\\Desktop\\Intelij Workspace\\Hello-World\\out\\artifacts\\Hello_World_jar\\Hello World.jar";
-	public static final String AGENT_PATH = PROJECT_DIR + "\\out\\artifacts\\Profiler_jar\\Profiler.jar";
-	public static final String AGENT_COMMAND = "-javaagent:" + AGENT_PATH;
-	public static String OUTSIDE_PROGRAM_COMMAND = "java -classpath \"C:\\Users\\David\\Desktop\\\"Intelij Workspace\"\\Excavator\\lib\\javassist.jar\" " +  AGENT_COMMAND + "\" -jar \"" + OUTSIDE_PROGRAM_JAR_PATH + "\"";
+	public static final String PROJECT_DIR        = System.getProperty("user.dir");
+	public static String OUTSIDE_PROGRAM_JAR_PATH = "C:\\Users\\David\\Desktop\\Intelij Workspace\\Hello-World\\out\\artifacts\\Hello_World_jar\\Hello World.jar";
+	public static final String AGENT_PATH         = PROJECT_DIR + "\\out\\artifacts\\Profiler_jar\\Profiler.jar";
+
+	public static final String AGENT_COMMAND = "-javaagent:" + "\"" + AGENT_PATH + "\"";
+	public static final String CLASSPATH_COMMAND = "-classpath \";C:\\Users\\David\\Desktop\\Intelij Workspace\\Excavator\\lib\\javassist.jar;\"";     // + PROJECT_DIR + "\\lib\\javassist.jar\"";
+
+	public static String OUTSIDE_PROGRAM_COMMAND = "java " + CLASSPATH_COMMAND + " " + AGENT_COMMAND + " -jar \"" + OUTSIDE_PROGRAM_JAR_PATH + "\"";
 
 
 	// The solution to the problem of the javaassist path is the \" \" aroung and subsection of the path that has a space in it
 	// TODO need a method that will convert paths to windows classpath usable format
+	// TODO Maybe use path object
 
 
+	public static String handlePathSpaces(String path)
+	{
+		String[] splitPath = path.split("\\\\");
 
+		StringBuilder stringBuilder = new StringBuilder();
 
+		for (String s : splitPath)
+		{
+			System.out.println(s);
 
+			if (s.contains(" "))
+			{
+				s = "\"" + s + "\"";
+			}
 
+			stringBuilder.append(s + "\\");
+		}
+
+		System.out.println(stringBuilder.toString());
+
+		return stringBuilder.toString().substring(0, stringBuilder.toString().length()-1);
+	}
 
 	////// RUN OPTIONS //////
 	public static final boolean SHOW_OUTSIDE_PROGRAM_OUTPUT = true;
